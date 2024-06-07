e8360d14b5bc1cd21412f3b85a7765a85a41ee71
socl0sed <letuchiy.force@gmail.com>
2024-06-07 10:12:38 +0000

    yml

---
+++ solver_application/build/CMakeFiles/equation.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/andrew/AndrewB203/workspacenew/lab04/solver_application/equation.cpp" "CMakeFiles/equation.dir/equation.cpp.o" "gcc" "CMakeFiles/equation.dir/equation.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/andrew/AndrewB203/workspacenew/lab04/solver_application/build/CMakeFiles/formatter_ex.dir/DependInfo.cmake"
+  "/home/andrew/AndrewB203/workspacenew/lab04/solver_application/build/CMakeFiles/solver.dir/DependInfo.cmake"
+  "/home/andrew/AndrewB203/workspacenew/lab04/solver_application/build/CMakeFiles/formatter.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ solver_application/build/CMakeFiles/solver.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/solver.dir/home/andrew/AndrewB203/workspacenew/lab04/solver_lib/solver.cpp.o"
+  "CMakeFiles/solver.dir/home/andrew/AndrewB203/workspacenew/lab04/solver_lib/solver.cpp.o.d"
+  "libsolver.a"
+  "libsolver.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/solver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
